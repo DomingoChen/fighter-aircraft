d76a0edc385e6ff339eebe36a9fa6db4bad5fa30
DomingoChen <51066391+DomingoChen@users.noreply.github.com>
2020-06-27 13:42:33 +0800

    變數改成中文

--- main.ts
+++ main.ts
@@ -1,32 +1,53 @@
 input.onButtonPressed(Button.A, function () {
-    if (gameStatus == 2) {
-        if (myXSpot > 0) {
-            led.unplot(myXSpot, myYSpot)
-            myXSpot = myXSpot - 1
-            led.plot(myXSpot, myYSpot)
+    if (遊戲狀態 == "開始") {
+        if (我的X座標 > 0) {
+            led.unplot(我的X座標, 我的Y座標)
+            我的X座標 = 我的X座標 - 1
+            led.plot(我的X座標, 我的Y座標)
         }
     }
 })
-function intervalFire () {
-    while (gameStatus == 2) {
-        goDown()
+function 一個攻擊 () {
+    攻擊的X座標 = randint(0, 4)
+    攻擊的Y座標 = 0
+    for (let index = 0; index <= 4; index++) {
+        if (確認是否擊中(攻擊的X座標, 攻擊的Y座標) == true) {
+            遊戲狀態 = "結束"
+            basic.showIcon(IconNames.No)
+            break;
+        } else {
+            led.plot(攻擊的X座標, 攻擊的Y座標)
+            basic.pause(200)
+            if (確認是否擊中(攻擊的X座標, 攻擊的Y座標) == false) {
+                led.unplot(攻擊的X座標, 攻擊的Y座標)
+            }
+            if (index < 4) {
+                攻擊的Y座標 = 攻擊的Y座標 + 1
+            }
+        }
     }
 }
-function checkIfHit (num: number, num2: number) {
-    if (num == myXSpot && num2 == 4) {
+function 確認是否擊中 (num: number, num2: number) {
+    if (num == 我的X座標 && num2 == 4) {
         return true
     }
     return false
 }
+function 重複攻擊 () {
+    while (遊戲狀態 == "開始") {
+        一個攻擊()
+    }
+}
 input.onButtonPressed(Button.AB, function () {
-    if (gameStatus == 1) {
-        led.plot(myXSpot, myYSpot)
-        gameStatus = 2
-        intervalFire()
-    } else if (gameStatus == 2) {
-        led.unplot(myXSpot, myYSpot)
-        gameStatus = 1
+    if (遊戲狀態 == "停止") {
+        led.plot(我的X座標, 我的Y座標)
+        遊戲狀態 = "開始"
+        重複攻擊()
+    } else if (遊戲狀態 == "開始") {
+        led.stopAnimation()
+        遊戲狀態 = "停止"
     } else {
+        led.stopAnimation()
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -34,47 +55,33 @@ input.onButtonPressed(Button.AB, function () {
             . . . . .
             . . . . .
             `)
-        gameStatus = 1
+        遊戲狀態 = "停止"
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (gameStatus == 2) {
-        if (myXSpot < 4) {
-            led.unplot(myXSpot, myYSpot)
-            myXSpot = myXSpot + 1
-            led.plot(myXSpot, myYSpot)
+    if (遊戲狀態 == "開始") {
+        if (我的X座標 < 4) {
+            led.unplot(我的X座標, 我的Y座標)
+            我的X座標 = 我的X座標 + 1
+            led.plot(我的X座標, 我的Y座標)
         }
     }
 })
-function goDown () {
-    shineXSpot = randint(0, 4)
-    shineYSpot = 0
-    for (let index = 0; index <= 4; index++) {
-        if (checkIfHit(shineXSpot, shineYSpot)) {
-            gameStatus = 0
-            basic.showIcon(IconNames.No)
-            break;
-        } else {
-            led.plot(shineXSpot, shineYSpot)
-            basic.pause(200)
-            if (checkIfHit(shineXSpot, shineYSpot) == false) {
-                led.unplot(shineXSpot, shineYSpot)
-            }
-            if (index != 4) {
-                shineYSpot = shineYSpot + 1
-            }
-        }
-    }
-}
-let shineYSpot = 0
-let shineXSpot = 0
-let myYSpot = 0
-let myXSpot = 0
-let gameStatus = 0
+let 攻擊的Y座標 = 0
+let 攻擊的X座標 = 0
+let 我的Y座標 = 0
+let 我的X座標 = 0
+let 遊戲狀態 = ""
 // 0:over, 1:stop, 2:start
-gameStatus = 1
-myXSpot = 2
-myYSpot = 4
-if (gameStatus == 1) {
-    basic.showString("A+B!")
+遊戲狀態 = "停止"
+我的X座標 = 2
+我的Y座標 = 4
+if (遊戲狀態 == "停止") {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 }
